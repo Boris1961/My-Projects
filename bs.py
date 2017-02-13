457344dabba14caa9463bf37710578343cf019c2
Boris1961 <borlov@inbox.ru>
2017-02-14 01:51:03 +0300

    input

--- bs.py
+++ bs.py
@@ -6,8 +6,8 @@ import requests
 # CSS_selector
 
 paths = {
-    'laminat33.ru': ['div.prd-wrapper', 'span[itemprop="name"]', 'span.price'],
-    'bikeparts.pythonanywhere.com' : ['div.panel', 'div.b-title', 'div.b-price'],
+    'laminat33.ru': ['div.prd-wrapper', 'img[src]', 'span[itemprop="name"]', 'span.price'],
+    'bikeparts.pythonanywhere.com' : ['div.panel', '', 'div.b-title', 'div.b-price'],
     'laminat-msc.ru' : []
 }
 
@@ -17,54 +17,36 @@ app = Flask(__name__)
 def index():
     return render_template("inp.html")
 
-@app.route("/search/", methods=["post"])
+@app.route("/search", methods=["post"])
 def search():
 
     key_word = request.form.get("goods")
     url = request.form.get("site")
 
-    print(key_word, url)
-
     # key_word = 'Balterio'
     # url = "https://laminat33.ru/category/laminat/balterio/?page=5"
 
-    # key_word = 'Vinca'
-    # url = "http://bikeparts.pythonanywhere.com/"
+    netloc = urlparse(url).netloc.lower()
+    if paths.get(netloc) == None:
+        key_word = 'Balterio'
+        url = "https://laminat33.ru/category/laminat/balterio/?page=5"
+
+    netloc = urlparse(url).scheme.lower() + ':\\' + urlparse(url).netloc.lower()
+    sel_items, sel_img, sel_goods, sel_price = paths[netloc]
 
     try:
-        sel_items, sel_goods, sel_price = paths [ urlparse(url).netloc.lower() ]
-    except KeyError:
+        response = requests.get(url)
+    except:
         return
 
-    response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
 
     need = [tag for tag in soup.select(sel_items) if tag.text.find(key_word) > 0]
 
-    html_str  = """  '<div class="wa-field wa-field-email">
-                <div class="wa-name"> URL </div>
-                <div class="wa-value">
-                <input type="text" name="url" value=""> </div> </div>'
-                """
-
-    html_str = '<table width="500" bgcolor="#c0c0c0" cellspacing="0" cellpadding="5" border="1" align="left"> <caption> Товар: '
-    html_str += key_word
-    html_str += '</caption>  <tr> <td> Описание </td> <td> Цена </td> </tr>'
-
-
-    for tag in need:
-        html_str += '<tr> <td> ' + tag.select(sel_goods)[0].text + ' </td> <td>' + tag.select(sel_price)[0].text + '</td> </tr>'
-
-    html_str += ' </table>'
-
-    # print(BeautifulSoup(html_str).contents, "html.parser")
-
-    # return html_str
-    key_list = [ [tag.select(sel_goods)[0].text, tag.select(sel_price)[0].text] for tag in need]
+    key_list = [ [ netloc+tag.select(sel_img)[0]['src'], tag.select(sel_goods)[0].text, tag.select(sel_price)[0].text ] for tag in need ]
 
     return render_template("bs_index.html", key = key_word, list = key_list)
 
 if __name__ == "__main__":
 
     app.run()
-
--- bs_tst.py
+++ bs_tst.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 
 def start ():
-    response = requests.get("http://bikeparts.pythonanywhere.com/")
-    # response = requests.get("https://laminat33.ru/category/laminat/balterio/?page=5")
+    # response = requests.get("http://bikeparts.pythonanywhere.com/")
+    response = requests.get("https://laminat33.ru/category/laminat/balterio/?page=5")
     soup = BeautifulSoup(response.content, 'html.parser')
     return soup
